d6717e73cb0337abc30b0a63e06768facbf0fe53
Arsenio Cantuangco Jr <abcantuangco@gmail.com>
2016-06-10 14:17:42 +0800

    add subscribe option + add generic template

--- index.php
+++ index.php
@@ -21,6 +21,30 @@ class MessengerBot {
         $this->access_token = getenv('ACCESS_TOKEN');
     }
 
+    public function subscribe() {
+        $url = $this->api . 'me/subscribed_apps?access_token=' . $this->access_token;
+
+        try {
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER , false);
+            //curl_setopt($ch, CURLOPT_SSL_VERIFYHOST , 2);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+            $result = curl_exec($ch);
+
+            if ($result === false) {
+                $this->log( curl_error($ch) );
+            } else {
+                $this->log( 'You have successfully subscribe to the App.' );
+            }
+
+            curl_close($ch);
+        } catch (Exception $e) {
+            $this->log( 'Error doing CURL request.' );
+        }
+    }
+
     public function listen()
     {
         $this->chat_message = json_decode(file_get_contents('php://input'), true);
@@ -45,6 +69,7 @@ class MessengerBot {
 
         $image = strpos( strtolower(trim($message)), 'image' );
         $buttons = strpos( strtolower(trim($message)), 'button' );
+        $generic = strpos( strtolower(trim($message)), 'generic' );
 
         if ($hi !== false) {
             return $this->generateMessageBody(['text' => 'Hello there!']);
@@ -65,6 +90,10 @@ class MessengerBot {
                 'postback_title' => 'Click for an Image',
                 'postback' => 'image'
                 ], 'buttons');
+        }
+
+        if ($generic !== false) {
+            return $this->generateMessageBody(array(), 'generic');
         }
 
     }
@@ -103,6 +132,50 @@ class MessengerBot {
                                   }
                                 }';
                 break;
+            case 'generic':
+                $template = '"attachment": {
+                                "type":"template",
+                                "payload":{
+                                    "template_type":"generic",
+                                    "elements":[
+                                        {
+                                            "title":"Article 01",
+                                            "image_url":"http://lorempixel.com/400/400/sports/",
+                                            "subtitle":"Sample article 01 here for testing.",
+                                            "buttons":[
+                                                {
+                                                    "type":"web_url",
+                                                    "url":"http://www.gmanetwork.com/news",
+                                                    "title":"View on GNO"
+                                                },
+                                                {
+                                                    "type":"postback",
+                                                    "title":"See Image",
+                                                    "payload":"image"
+                                                }              
+                                            ]
+                                        },
+                                        {
+                                            "title":"Article 02",
+                                            "image_url":"http://lorempixel.com/400/400/sports/",
+                                            "subtitle":"Sample article 02 here for testing.",
+                                            "buttons":[
+                                                {
+                                                    "type":"web_url",
+                                                    "url":"http://www.gmanetwork.com/news",
+                                                    "title":"View on GNO"
+                                                },
+                                                {
+                                                    "type":"postback",
+                                                    "title":"See Image",
+                                                    "payload":"image"
+                                                }              
+                                            ]
+                                        }
+                                    ]
+                                }
+                            }';
+                break;
             default:
                 if ($data->text) {
                     $template = '"text":"' . $data->text . '"';
@@ -116,8 +189,6 @@ class MessengerBot {
     public function respond() {
         //API Url
         $url = $this->api . 'me/messages?access_token=' . $this->access_token;
-
-        $this->log( $url );
 
         /*//The JSON data.
         $jsonData = array(
@@ -193,7 +264,12 @@ $chatApp->listen();
 
 if (isset($_GET['hub_verify_token']) && $_GET['hub_verify_token'] === "gno_chat_test_token") {
     $chatApp->inputs = $_GET;
+    $chatApp->log($chatApp->inputs);
     echo $chatApp->verify();
+}
+
+if (isset($_GET['subscribe'])) {
+    $chatApp->subscribe();
 }
 
 /*$query = $_POST;
